bc0cdd6d0b72c2c1abacc904c489624d17b72240
Luciano <LucianoPSN15@gmail.com>
2023-12-06 14:14:12 +0100

    minor changes to monster project (functional components)

--- src/App.js
+++ src/App.js
@@ -4,46 +4,50 @@ import Header from './Components/header/Header';
 import SearchBox from './Components/Search-Box/SearchBoxComponent';
 
 export default function App() {
-  //useState Hook for the search field
-  const [searchField, setSearchField] = useState('');
-  const [monsters, setMonsters] = useState([]);
-  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+     //useState Hook for the search field
+     const [monsters, setMonsters] = useState([]);
+     const [searchField, setSearchField] = useState('');
+     const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-  //useEffect is a side effect(meaning it relies on objects outside its scope), this hook take in a callback function(what we want to happen in the code)
-  // the second parameter in useEffect takes in dependecies, e.g prop values or state values
-  //the callBack function only runs when the depency changes whilst rendering
-  useEffect(() => {
-    console.log('useEffect');
-    //first we fetch data from remote place using 'fetch
-    fetch('https://jsonplaceholder.typicode.com/users')
-      //then we take the response from the site and turn it into json
-      .then((response) => response.json())
-      // then we take the first ".then" and pass "users" and callback "setMonsters" to return users inside our state "monsters" array
-      .then((users) => setMonsters(users));
-  }, []);
+     //useEffect is a side effect(meaning it relies on objects of outside its scope), this hook takes in a callback function(what we want to happen in the code)
+     // the second parameter in useEffect takes in dependecies, e.g prop values or state values
+     //the callBack function only runs when the depency changes whilst rendering
+     useEffect(() => {
+          //this console log is just to see how many times useEffect is rendered
+          console.log('useEffect');
+          //first we fetch data from remote place using 'fetch
+          fetch('https://jsonplaceholder.typicode.com/users')
+               //then we take the response from the site and turn it into json
+               .then(response => response.json())
+               // then we take the first ".then" and pass "users" and callback "setMonsters" to return users inside our state "monsters" array
+               .then(users => setMonsters(users));
+     }, []);
 
-  //another useEffect for filtered monsters
-  useEffect(() => {
-    const newFilteredMonsters = monsters.filter((monstersNames) => {
-      return monstersNames.name.toLocaleLowerCase().includes(searchField);
-    });
-    setFilteredMonsters(newFilteredMonsters);
-  }, [monsters, searchField]);
+     //another useEffect for filtered monsters
+     useEffect(() => {
+          //the first parameter takes in a function for searching our monsters, first creat the arrow function thenn follow up with a filter and return the parameter in lower case and make sure it includes the search field
+          const newFilteredMonsters = monsters.filter(monstersNames => {
+               return monstersNames.name
+                    .toLocaleLowerCase()
+                    .includes(searchField);
+          });
+          setFilteredMonsters(newFilteredMonsters);
+     }, [monsters, searchField]);
 
-  const onSearchChange = (event) => {
-    const searchFieldString = event.target.value.toLocaleLowerCase();
-    setSearchField(searchFieldString);
-  };
+     const onSearchChange = event => {
+          const searchFieldString = event.target.value.toLocaleLowerCase();
+          setSearchField(searchFieldString);
+     };
 
-  return (
-    <div className="App">
-      <Header className="header" />
-      <SearchBox
-        onChangeHandler={onSearchChange}
-        placeHolder="Search the monsters"
-        className="monsters-searchbox"
-      />
-      <CardList monsters={filteredMonsters} />
-    </div>
-  );
+     return (
+          <div className="App">
+               <Header className="header" />
+               <SearchBox
+                    onChangeHandler={onSearchChange}
+                    placeHolder="Search the monsters"
+                    className="monsters-searchbox"
+               />
+               <CardList monsters={filteredMonsters} />
+          </div>
+     );
 }
--- src/Components/Search-Box/SearchBoxComponent.js
+++ src/Components/Search-Box/SearchBoxComponent.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './css/Search-box.css';
 
 export default function SearchBox(props) {
-  return (
-    <input
-      type="search"
-      className={`search-box ${props.className}`}
-      placeholder={props.placeHolder}
-      onChange={props.onChangeHandler}
-    />
-  );
+     const { className, placeHolder, onChangeHandler } = props;
+     return (
+          <input
+               type="search"
+               className={`search-box ${className}`}
+               placeholder={placeHolder}
+               onChange={onChangeHandler}
+          />
+     );
 }
